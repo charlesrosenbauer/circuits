df9c22cbdfc40b63c6bc6f078852a4fb4cf140b0
Charles <charles.rosenbauer@gmail.com>
2023-01-27 23:04:58 -0600

    Some work on circuit simulation

--- src/sim.c
+++ src/sim.c
@@ -15,17 +15,32 @@ void simulatorStep(Circuit* c){
 			* simulate gates
 			* account for changing wires
 		*/
-		switch(c->gates[i].k){
-			case G_EQ	: break;
-			case G_NOT	: break;
-			case G_AND	: break;
-			case G_NAND	: break;
-			case G_OR	: break;
-			case G_NOR	: break;
-			case G_XOR	: break;
-			case G_XNOR	: break;
-			case G_SLAT	: break;
-			case G_DLAT	: break;
+		Gate g = c->gates[i];
+		if((g.k == G_SLAT) || (g.k == G_DLAT)){
+			// MEMORY
+		}else{
+			int aix = g.a;
+			int bix = g.b;
+			int qix = g.c;
+			for(int j = 0; j < g.width; j += 64){
+				// TODO: handle cases for unaligned bits
+				uint64_t a, b, q;
+				a = c->wirebits[aix / 64];
+				b = c->wirebits[bix / 64];
+				
+				switch(g.k){
+					case G_EQ	: q =       a ; break;
+					case G_NOT	: q =      ~a ; break;
+					case G_AND	: q =   a & b ; break;
+					case G_NAND	: q = ~(a & b); break;
+					case G_OR	: q =   a | b ; break;
+					case G_NOR	: q = ~(a | b); break;
+					case G_XOR	: q =   a ^ b ; break;
+					case G_XNOR	: q = ~(a ^ b); break;
+				}
+				
+				c->wirebits[qix/64] = q;
+			}
 		}
 	}
 }
